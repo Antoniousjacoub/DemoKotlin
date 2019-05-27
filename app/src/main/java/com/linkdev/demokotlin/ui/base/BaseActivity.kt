f12a30f8fc7e76559b3743afa5206662aca5dabe
Antonious <antonious.yacoub@linkdev.com>
2019-05-27 15:17:54 +0200

    new branch

--- app/src/main/java/com/linkdev/demokotlin/ui/base/BaseActivity.kt
+++ app/src/main/java/com/linkdev/demokotlin/ui/base/BaseActivity.kt
@@ -11,16 +11,6 @@ import com.linkdev.demokotlin.R
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    interface Comparable<in T> {
-        operator fun compareTo(other: T): Int
-    }
-
-    fun demo(x: Comparable<Number>) {
-        x.compareTo(1.0) // 1.0 has type Double, which is a subtype of Number
-        // Thus, we can assign x to a variable of type Comparable<Double>
-        val y: Comparable<Int> = x // OK!
-    }
-
     private lateinit var myToolbar: Toolbar
 
     @LayoutRes
