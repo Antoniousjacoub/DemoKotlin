4752446fb708d24d983a7bc44775eca3d790c051
Antonious <antonious.yacoub@linkdev.com>
2019-05-30 11:50:22 +0200

    handle location screen

--- app/src/main/java/com/linkdev/demokotlin/retrofit/NetworkingModule.kt
+++ app/src/main/java/com/linkdev/demokotlin/retrofit/NetworkingModule.kt
@@ -2,7 +2,6 @@ package com.linkdev.demokotlin.retrofit
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.linkdev.demokotlin.common.helpers.Constants
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,19 +9,6 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 object NetworkingModule {
 
-    //Creating Auth Interceptor to add api_key query in front of all the requests.
-    private val authInterceptor = Interceptor { chain ->
-        val newUrl = chain.request().url()
-            .newBuilder()
-            .build()
-
-        val newRequest = chain.request()
-            .newBuilder()
-            .url(newUrl)
-            .build()
-
-        chain.proceed(newRequest)
-    }
     private val interceptorBody: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
         this.level = HttpLoggingInterceptor.Level.BODY
     }
@@ -34,7 +20,6 @@ object NetworkingModule {
     private val okHttpClient = OkHttpClient().newBuilder()
         .addInterceptor(interceptorHeader)
         .addInterceptor(interceptorBody)
-        .addInterceptor(authInterceptor)
         .build()
 
 
